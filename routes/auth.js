4477e9869547988d5ebd5f77683e690fb46d3065
Okononfua Mathew <oseghaleokononfua@gmail.com>
2023-08-28 14:09:08 +0100

    second commit

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,9 @@ const router = express.Router();
 
 router.get('/login', authController.getLogin);
 
-router.get('/signup', authController.getSignup)
+router.get('/signup', authController.getSignup);
+
+router.post('/signup', authController.postSignup);
 
 router.post('/login', authController.postLogin);
 
